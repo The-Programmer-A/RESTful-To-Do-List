c198951ccf25bc645603df186cf41e0dd2c3ba55
chandrarma <chandrarma@ecs.vuw.ac.nz>
2019-09-10 17:05:45 +1200

    soemthign

--- index.js
+++ index.js
@@ -38,33 +38,7 @@ express()
       console.error(err);
       res.send("Error " + err);
     }
-
-    .post("/items", function(req, res) {
-    //res.send("You are in the API call" + req.body.task + " " + req.body.name);
-    try {
-      //const client = await pool.connect();
-      //const result = await client.query("SELECT * FROM todo");
-      //const results = { results: result ? result.rows : null }; //else { return res.send('No Data Found')}
-      //res.render("db", results);
-      //client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
   })
-
-  .get("/test", function(req, res) {
-    //res.send("You are in the API call" + req.body.task + " " + req.body.name);
-    try {
-      //const client = await pool.connect();
-      //const result = await client.query("SELECT * FROM todo");
-      //const results = { results: result ? result.rows : null }; //else { return res.send('No Data Found')}
-      //res.render("db", results);
-      //client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
   //INSERT INTO todo(item, username, status) VALUES('something','Armaan', '0');
   // Implement appropriate database calls for each API function of your RESTful web service.
   .get("/cool", (req, res) => res.send(cool()))
